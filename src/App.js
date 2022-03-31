5ada7204bf0c7caccd024f028f31ac67b193e5de
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-03-31 12:59:39 +0300

    feat: create checking of Metamusk userProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+
 
 function App() {
+
+  function detectAuthUser() {
+    let userProvider;
+    if (window.etherium) {
+      userProvider = window.etherium;
+    } else if (window.web3) {
+      userProvider = window.web3.currentProvider;
+    } else {
+      console.log('you are not logined with MetaMusk wallet')
+    }
+    return userProvider;
+  }
+
+  useEffect(() => {
+    console.log('eth:', detectAuthUser());
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { }
         </p>
         <a
           className="App-link"
