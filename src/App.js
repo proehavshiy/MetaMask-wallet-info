d86322353f2244c3de161b0a844bafddd2763316
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-04-01 00:56:11 +0300

    feat: implement onboarding lib

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import pic from './img/MetaMaskLogo.png'
 import './App.css';
 import { useEffect, useState } from 'react'
 import Web3 from 'web3';
-import WalletInfo from './components/WalletInfo';
+import MetaMaskOnboarding from '@metamask/onboarding';
+import WalletInfo from './components/WalletInfo/WalletInfo';
 import { NotLoginedError } from './errors/NotLoginedError';
 import { CONSTANTS_PHRASES } from './constants/constants'
 
-
 function App() {
-  const { NOTLOGINED, LOGINED, BADCONNECTION, BTN_OK, BTN_CONNECTING, BTN_LOGOUT } = CONSTANTS_PHRASES
+  const { NOTLOGINED, LOGINED, BADCONNECTION, BTN_OK, BTN_CONNECTING, BTN_LOGOUT, BTN_INSTALL } = CONSTANTS_PHRASES
 
   const [userInfo, setUserInfo] = useState({})
   const [isConnected, setIsConnected] = useState(false)
   const [isBtnDisabled, setIsBtnDisabled] = useState(false)
   const [statusTitle, setStatusTitle] = useState(NOTLOGINED)
+  const [isOnboarding, setIsOnboarding] = useState(false)
 
-  function detectMetamaskExtension() {
+  const onboarding = new MetaMaskOnboarding()
+
+  function getProvider() {
     let ethProvider;
     const { etherium, web3 } = window;
     if (etherium) {
-      ethProvider = etherium;
+      ethProvider = window.etherium;
     } else if (web3) {
       ethProvider = web3.currentProvider;
     } else {
@@ -28,19 +31,11 @@ function App() {
     return ethProvider;
   }
 
-  // check MetaMask installation status
-  useEffect(() => {
-    const { web3 } = window;
-    !web3
-      ? setStatusTitle(NOTLOGINED)
-      : setStatusTitle(LOGINED)
-  }, [])
-
   async function connectToMetaMask() {
     try {
       // disable button
       setIsBtnDisabled(true)
-      const userProvider = detectMetamaskExtension()
+      const userProvider = getProvider()
       if (!userProvider) {
         throw new NotLoginedError()
       }
@@ -103,13 +98,36 @@ function App() {
     setIsConnected(true)
   }
 
-  const buttonTitle = !isConnected
-    ? (!isBtnDisabled ? BTN_OK : BTN_CONNECTING)
-    : BTN_LOGOUT
+  // set status title
+  useEffect(() => {
+    !MetaMaskOnboarding.isMetaMaskInstalled()
+      ? setStatusTitle(NOTLOGINED)
+      : setStatusTitle(LOGINED)
+  }, [])
+
+  const setBtnTitle = () => {
+    return MetaMaskOnboarding.isMetaMaskInstalled()
+      ? (!isConnected ? (!isBtnDisabled ? BTN_OK : BTN_CONNECTING) : BTN_LOGOUT)
+      : BTN_INSTALL
+  }
 
   function onClick() {
-    return !isConnected ? connectToMetaMask() : disconnectMetaMask()
-  }
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      return !isConnected ? connectToMetaMask() : disconnectMetaMask()
+    } else {
+      // install MetaMask extension by startOnboarding
+      onboarding.startOnboarding();
+      setIsOnboarding(true)
+    }
+  };
+
+  // stop onboarding
+  useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      onboarding.stopOnboarding();
+      setIsOnboarding(false)
+    }
+  }, [isOnboarding]);
 
   return (
     <main>
@@ -125,14 +143,13 @@ function App() {
           }
           <div className='control'>
             <button className='button' onClick={onClick} disabled={isBtnDisabled}>
-              {buttonTitle}
+              {setBtnTitle()}
             </button>
             {
               !isConnected &&
               <p className='status'>{statusTitle}</p>
             }
           </div>
-
         </div>
       </div>
     </main>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,9 @@
 export const CONSTANTS_PHRASES = {
-  NOTLOGINED: 'MetaMask is locked. Please login',
+  NOTLOGINED: 'MetaMask is locked. Please install and login',
   LOGINED: 'MetaMask is available!',
   BADCONNECTION: 'Something wrong with the connection. Try later.',
   BTN_OK: 'Get public key',
   BTN_CONNECTING: 'connecting...',
   BTN_LOGOUT: 'Log Out',
+  BTN_INSTALL: 'Install MetaMask',
 }
