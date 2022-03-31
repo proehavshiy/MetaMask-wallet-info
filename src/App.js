1bf5884aba2f6ef74555c1269d3b77423c52c43a
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-03-31 20:32:16 +0300

    feat: add btn status

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import Web3 from 'web3';
 import { v4 as uuidv4 } from 'uuid';
+import { CONSTANTS_PHRASES } from './constants/constants'
 
 
 function App() {
-  const [isMetaMaskInstalled, setIsMetaMaskInstalled] = useState(null)
+  const { NOTLOGINED, LOGINED, BADCONNECTION, BTN_OK, BTN_CONNECTING, BTN_LOGOUT } = CONSTANTS_PHRASES
+
   const [userInfo, setUserInfo] = useState({})
   const [isConnected, setIsConnected] = useState(false)
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false)
+  const [statusTitle, setStatusTitle] = useState(NOTLOGINED)
 
   function detectMetamaskExtension() {
     let ethProvider;
@@ -18,7 +22,7 @@ function App() {
     } else if (web3) {
       ethProvider = web3.currentProvider;
     } else {
-      console.log('you are not logined with MetaMusk wallet');
+      setStatusTitle(NOTLOGINED)
     }
     return ethProvider;
   }
@@ -26,12 +30,16 @@ function App() {
   // check MetaMask installation status
   useEffect(() => {
     const { web3 } = window;
-    !web3 ? setIsMetaMaskInstalled(false) : setIsMetaMaskInstalled(true)
+    !web3
+      ? setStatusTitle(NOTLOGINED)
+      : setStatusTitle(LOGINED)
   }, [])
 
 
   async function connectToMetaMask() {
     try {
+      // disable button
+      setIsBtnDisabled(true)
       const userProvider = detectMetamaskExtension()
 
       // You should only initiate a connection request in response to direct user action,
@@ -39,21 +47,28 @@ function App() {
       // You should never initiate a connection request on page load.
       const web3 = new Web3(userProvider) // web3 main class
 
-      // get acc info
       //access the user's Ethereum account(s)
       const publicKey = await userProvider.request({ method: 'eth_requestAccounts' });
       // Get wallet balance
       let ethBalance = await web3.eth.getBalance(publicKey[0]);
-      console.log('ethBalance:', ethBalance);
-      // save to localStorage
+      // ...other details
+
+      //save to localStorage
       saveInfoToLocalStorage([
         {
           name: 'Public key',
           data: publicKey[0],
         },
+        {
+          name: 'balance',
+          data: ethBalance,
+        },
       ]);
     } catch (err) {
-      console.log(err)
+      setStatusTitle(BADCONNECTION)
+    } finally {
+      // enable button
+      setIsBtnDisabled(false)
     }
   }
 
@@ -81,32 +96,35 @@ function App() {
     setIsConnected(true)
   }
 
-  const buttonTitle = !isConnected ? 'Get public key' : 'Log out'
-  const statusTitle = !isMetaMaskInstalled ? 'MetaMusk is locked. Please login' : 'MetaMusk is available!'
+  const buttonTitle = !isConnected
+    ? (!isBtnDisabled ? BTN_OK : BTN_CONNECTING)
+    : BTN_LOGOUT
 
   function onClick() {
     return !isConnected ? connectToMetaMask() : disconnectMetaMask()
   }
 
-
-
   return (
     <div className="App">
       <main>
         <div className='wrapper'>
-          <h1>Check your MetaMusk Wallet stats</h1>
+          <h1>Check your MetaMusk Wallet info</h1>
           {
             isConnected &&
             <>
-              <h2 className='heading'>Account details:</h2>
+              <h2 className='heading'>Details:</h2>
               <ul className='wallet-info'>
                 {
-                  userInfo.map(item => <li className='wallet-info__item' key={uuidv4()}>{item.name}: {item.data}</li>)
+                  userInfo.map(item =>
+                    <li className='wallet-info__item' key={uuidv4()}>
+                      {item.name}: {item.data}
+                    </li>
+                  )
                 }
               </ul>
             </>
           }
-          <button className='button' onClick={onClick}>
+          <button className='button' onClick={onClick} disabled={isBtnDisabled}>
             {buttonTitle}
           </button>
           {
---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+export const CONSTANTS_PHRASES = {
+  NOTLOGINED: 'MetaMusk is locked. Please login',
+  LOGINED: 'MetaMusk is available!',
+  BADCONNECTION: 'Something wrong with the connection. Try later.',
+  BTN_OK: 'Get public key',
+  BTN_CONNECTING: 'connecting...',
+  BTN_LOGOUT: 'Log Out',
+}
